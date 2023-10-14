39dce80abd58c1eec331d3c742966d45a58588e5
Kent Miguel <kentmiguel10@gmail.com>
2023-10-14 15:12:21 -0400

    Style active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { Bug } from "lucide-react";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 export default function NavBar() {
+  const currentPath = usePathname();
+  console.log(currentPath);
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -10,7 +17,6 @@ export default function NavBar() {
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
-        {" "}
         <Bug />
       </Link>
       <ul className="flex space-x-6">
@@ -18,7 +24,11 @@ export default function NavBar() {
           <Link
             key={link.href}
             href={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             {link.label}
           </Link>
