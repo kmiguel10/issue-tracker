1420e3418cecd9640af333fd1da75642516659a7
Kent Miguel <kentmiguel10@gmail.com>
2023-11-17 14:05:30 -0500

    Filter Issues

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,6 +2,7 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const statuses: { label: string; value?: Status }[] = [
@@ -12,8 +13,14 @@ const statuses: { label: string; value?: Status }[] = [
 ];
 
 export default function IssueStatusFilter() {
+  const router = useRouter();
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(status) => {
+        const query = status === "ALL" ? "" : `?status=${status}`;
+        router.push(`/issues/list` + query);
+      }}
+    >
       <Select.Trigger>Filter by status...</Select.Trigger>
       <Select.Content>
         {statuses.map((status) => (
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,9 +2,22 @@ import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import IssueActions from "./IssueActions";
+import { Status } from "@prisma/client";
 
-export default async function IssuesPage() {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: { status: Status };
+}
+
+export default async function IssuesPage({ searchParams }: Props) {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+  const issues = await prisma.issue.findMany({
+    where: { status },
+  });
+
+  console.log(searchParams.status);
 
   return (
     <div>
